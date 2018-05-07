68ceae8d1e4ebca4d03c840e84b6969df09cb1e4
Muhtar Arfa <muhtarrn@gmail.com>
2018-05-07 11:44:10 +0700

    Update data_preprocessing_template.py

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -30,7 +30,7 @@ from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, 
                                                     random_state = 0)
 
-#Feature scaling
+# Feature scaling
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
