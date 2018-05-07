2cab31fbdb1d2a5cdf8a5e0cf678b820770f6b71
Muhtar Arfa <muhtarrn@gmail.com>
2018-05-07 11:44:52 +0700

    Update data_preprocessing_template.R

--- data_preprocessing_template.R
+++ data_preprocessing_template.R
@@ -19,7 +19,7 @@ dataset$Purchased = factor(dataset$Purchased,
                            levels = c('No', 'Yes'),
                            labels = c(0, 1))
 
-# Splitting the dataset into the Training set and Test test
+# Splitting the dataset into the Training set and Test set
 #install.packages('caTools')
 library(caTools)
 set.seed(123)
@@ -27,6 +27,6 @@ split = sample.split(dataset$Purchased, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
-#Feature scaling
+# Feature scaling
 training_set[, 2:3] = scale(training_set[, 2:3])
 test_set[, 2:3] = scale(test_set[, 2:3])
